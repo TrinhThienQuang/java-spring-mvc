d5a676d993b9abbc8236ab8d4a048b55bc9c865c
TrinhThienQuang <24021611@vnu.edu.vn>
2026-01-02 00:03:50 +0700

    devtool and mysql

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "fuck you!";
+        return "Hello world !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "only user can access this site";
+    }
+    
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "only admin can access this site";
     }
 }
